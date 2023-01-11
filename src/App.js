124ad1f1c2fd4bfd568145e149531edbb56044e0
mdashik7723 <mdashik7723@gmail.com>
2023-01-12 00:23:21 +0600

    --

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
-function App() {
+
+
+ const App  = () => {
   return (
-    <div className="App">
-  <div>
-<h1> Hello MUI I Will Eat You </h1>
-  </div>
-    </div>
+
+      <div>
+          <Button variant="contained">Hello World</Button>
+      </div>
   );
 }
 
 export default App;
+
+
+
+
